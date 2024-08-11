3eb59941c0a00c714ba924e30ed97fb8b105be8e
Gianmarco Ciaffardini <ciaffardini.g@gmail.com>
2024-08-11 10:37:42 +0200

    build

---
+++ api/lokalise/webhooks/test.ts
@@ -0,0 +1,18 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+/**
+ * !TEST! Handles all TASK RELATED incoming webhooks from lokalise
+ * @param req
+ * @param res
+ * @returns
+ */
+export default async function POST(req: VercelRequest, res: VercelResponse) {
+  try {
+    return res.status(200).json({ body: req.body });
+  } catch (err: any) {
+    console.error(err);
+    return res
+      .status(400)
+      .json({ error: err?.message ?? "Something went wrong" });
+  }
+}
